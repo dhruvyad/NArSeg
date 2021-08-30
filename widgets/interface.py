eed7edf851bf3a67855d9f2fe644c0eb2cecf51e
dhruvyad <dhruv1103@gmail.com>
2021-08-31 04:06:55 +0530

    added basic button click handlers

--- widgets/interface.py
+++ widgets/interface.py
@@ -4,9 +4,11 @@ from kivy.uix.widget import Widget
 from utils import (get_mag_phase)
 import threading
 
-
-import matplotlib.pyplot as plt
 from kivy.clock import mainthread
+
+
+from kivy.uix.behaviors import ToggleButtonBehavior
+
 
 class NArSegInterface(Widget):
     magnitude = ObjectProperty(None)
@@ -14,12 +16,31 @@ class NArSegInterface(Widget):
     filedrop_timer, filedrop_window = None, 0.01 # will wait for 10 milliseconds for file
     current_files = []
 
+    view_buttons = None
+    artery_buttons = None
+
     def __init__(self, **kwargs):
         super(NArSegInterface, self).__init__(**kwargs)
+        # makes processing multiple files possible
         self.filedrop_timer = threading.Timer(self.filedrop_window, self.process_files)
+        # makes sure at least one toggle button is selected at all times
+        ToggleButtonBehavior.allow_no_selection = False
+        # initialize other variables
+        self.view_buttons = ToggleButtonBehavior.get_widgets('view_type')
+        for button in self.view_buttons:
+            button.bind(on_press=self.on_view_button_click)
+        self.artery_buttons = ToggleButtonBehavior.get_widgets('artery_select_type')
+        for button in self.artery_buttons:
+            button.bind(on_press=self.on_artery_button_click)
 
     def render(self):
         Window.bind(on_dropfile=self.filedrop)
+
+    def on_view_button_click(self, button):
+        print(button.text)
+
+    def on_artery_button_click(self, button):
+        print(button.text)
 
     def filedrop(self, _, file_path):
         if not self.filedrop_timer.is_alive():
